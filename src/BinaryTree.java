9532958299ab49d137dcbe989efc62b3bdeb422c
besweens <144698652+besweens@users.noreply.github.com>
2023-10-16 14:16:48 -0400

    initial push

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,45 +1,44 @@
 public class BinaryTree<Integer> {
     /**
-     * myBST enables the capability to build a binary tree of SalesRecord objects for use with the
-     * vehicle sales data received and processed by other classes.
-     * Will contain at least the following:
-     * boolean insert(SalesRecord sr) <-add to the tree
-     * int search(Date date) <- return the number of records on or after that date
-     * int recursiveSearch(Date date) <- return the number of records on or after that date, found with a recursive method
+     * Binary Tree class for Lab4M
+     * Utilizing nodes, binary trees can be built with a root and pointers for
+     * left and right children. Integer data is stored within the node.
      *
      * @author Sweeney
-     * @version 20231011
+     * @version 20231016
      */
-        protected Node<Integer> root;
+
+    //root is the top level of our binary tree
+    protected Node<Integer> root;
 
     /**
-     *
+     *Constructor for BinaryTree objects
      */
     public  BinaryTree(){
-            root = null;
-        }
+        root = null;
+    }
+
+    /**
+     * Internal class to build nodes for our binary tree.
+     * @param <Integer> nodes of type Integer
+     *
+     */
+    protected static class Node<Integer>{
+        Integer data;
+        Node<Integer> left;
+        Node<Integer> right;
 
         /**
-         * Internal class to build nodes for our binary tree.
-         * @param <Integer> nodes of type Integer
-         *
+         * Builder for nodes
+         * @param data
          */
-        protected static class Node<Integer>{
-            Integer data;
-            Node<Integer> left;
-            Node<Integer> right;
-
-            /**
-             * Builder for nodes
-             * @param data
-             */
-            public Node(Integer data) {
-                this.data = data;
-                left = null;
-                right = null;
-            }
-
+        public Node(Integer data) {
+            this.data = data;
+            left = null;
+            right = null;
         }
+
     }
+}
 
 
---
+++ src/README.md
@@ -0,0 +1,6 @@
+<h2>README - LAB 4M</h2>
+<p>Team Lead: Ben Sweeney<br>
+Subtask 1: Herve<br>
+Subtask 2: Audrey<br>
+Subtask 3: Zach<br>
+</p>
