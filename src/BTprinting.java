dd3f2e8553c44f7338591772b246c91ea6f96fcf
Hbeden4 <115094079+Hbeden4@users.noreply.github.com>
2023-10-16 21:02:20 -0400

    Update BTprinting.java

--- src/BTprinting.java
+++ src/BTprinting.java
@@ -1,4 +1,38 @@
 
-public class BTprinting extends BinaryTree<Integer> {
+public class BTPrinting extends BinaryTree {
+    public void postOrder() {
+        postOrder(root);
+    }
 
+    private void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.data + " ");
+    }
+
+    public void printRight() {
+        printRight(root, 0);
+    }
+
+    private void printRight(Node node, int level) {
+        if (node == null) {
+            return;
+        }
+
+        if (level == maxLevel) {
+            System.out.print(node.data + " ");
+            maxLevel++;
+        }
+
+        printRight(node.right, level + 1);
+        printRight(node.left, level + 1);
+    }
+
+    private int maxLevel = 0;
+
+    }
 }
